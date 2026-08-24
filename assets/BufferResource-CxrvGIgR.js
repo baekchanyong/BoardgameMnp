83e7f98e37b44548802bb5401c6793ded06ec7fa
baekchanyong <baekchanyong@users.noreply.github.com>
2026-08-24 23:39:00 +0900

    feat: lobby redesign (room list first, game picker modal, favorites, room detail)

--- assets/BufferResource-CxrvGIgR.js
+++ assets/BufferResource-CxrvGIgR.js
@@ -1,4 +1,4 @@
-import{w as U,g,c,u as S,B as _,d as m,E as I,e as p}from"./index-BLMHX16a.js";const x={name:"local-uniform-bit",vertex:{header:`
+import{w as U,g,c,u as S,B as _,d as m,E as I,e as p}from"./index-EM45xlnn.js";const x={name:"local-uniform-bit",vertex:{header:`
 
             struct LocalUniforms {
                 uTransformMatrix:mat3x3<f32>,
